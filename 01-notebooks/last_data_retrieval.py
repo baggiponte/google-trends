6db70b12e501c56d3e1e6cd280d5dc671186d173
baggiponte <lucabaggi.uni@gmail.com>
2021-09-14 16:13:35 +0200

    updated project structure

--- 01-notebooks/last_data_retrieval.py
+++ 01-notebooks/last_data_retrieval.py
@@ -6,14 +6,25 @@ end_date = "2021-08-31"
 
 pytrend = TrendReq()
 
-print("retrieve data for keyword 'euro'")
-euro = get_daily_trend(pytrend, "Euro", start_date, end_date, sleep_for=5)
+print("retrieve data for keyword 'euro', overlapping window 30")
+euro_30 = get_daily_trend(pytrend, "Euro", start_date, end_date, overlap=30)
 
-print("retrieve data for keyword 'european central bank'")
-ecb = get_daily_trend(
-    pytrend, "European Central Bank", start_date, end_date, sleep_for=5
+print("retrieve data for keyword 'euro', overlapping window 100")
+euro_100 = get_daily_trend(pytrend, "Euro", start_date, end_date, overlap=100)
+
+print("retrieve data for keyword 'european central bank', overlapping window 30")
+ecb_30 = get_daily_trend(
+    pytrend, "European Central Bank", start_date, end_date, overlap=30
+)
+
+print("retrieve data for keyword 'european central bank', overlapping window 100")
+ecb_100 = get_daily_trend(
+    pytrend, "European Central Bank", start_date, end_date, overlap=100
 )
 
 print("saving data to csv")
-euro.to_csv("./overlapping-daily-euro.csv")
-ecb.to_csv("./overlapping-daily-ecb.csv")
+euro_30.to_csv("./overlapping-daily-euro_30.csv")
+euro_100.to_csv("./overlapping-daily-euro_100.csv")
+
+ecb_30.to_csv("./overlapping-daily-ecb_30.csv")
+ecb_100.to_csv("./overlapping-daily-ecb_100.csv")
